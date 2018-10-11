4c1a724458d2ab0d53d3b49beb85c3180d957598
Martin Lehmann <martin@folio.no>
2018-10-11 15:09:50 +0200

    make it actually work

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 
 const preStyles = css`
   border: 5px solid green;
+  margin-left: 100px;
 `;
 
 const mediaStyles = css`
   @media (min-width: 720px) {
-    div {
-      margin: auto;
-    }
+    margin: 0;
   }
 `;
 
@@ -18,7 +17,8 @@ const postStyles = css`
 
 const a = document.createElement('div');
 a.innerHTML = 'hello';
-a.className = `${preStyles} ${mediaStyles} ${postStyles}`;
+a.className = cx(preStyles, mediaStyles, postStyles);
+// a.className = `${preStyles} ${mediaStyles} ${postStyles}`;
 
 const container = document.getElementById('container');
 
